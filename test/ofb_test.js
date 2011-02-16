f114571eb40c8234c63a20feb0f0f0aa2a679270
Christian Amor Kvalheim <christkv@gmail.com>
2011-02-16 17:17:51 +0100

    Basic OFB mode tests and encryption/decryption

--- test/ofb_test.js
+++ test/ofb_test.js
@@ -37,6 +37,64 @@ suite.addTests({
     var ofb = new OFBMode(new AESKey(key), new NullPad(), iv);
     var src = ofb.encrypt(pt);
     assert.deepEqual(ct, src);
+    
+    // Decrypt
+    var ofb = new OFBMode(new AESKey(key), new NullPad(), iv);
+    var decrypted = ofb.decrypt(src);
+    assert.deepEqual(pt, decrypted);
+    finished();
+  },  
+
+  "OFB AES-192 Test Vectors":function(assert, finished) {
+    var key = util.hexStringToBinaryArray("8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b");
+    var pt = util.hexStringToBinaryArray(
+			"6bc1bee22e409f96e93d7e117393172a" + 
+			"ae2d8a571e03ac9c9eb76fac45af8e51" + 
+			"30c81c46a35ce411e5fbc1191a0a52ef" + 
+			"f69f2445df4f9b17ad2b417be66c3710");
+    var ct = util.hexStringToBinaryArray(
+			"cdc80d6fddf18cab34c25909c99a4174" + 
+			"fcc28b8d4c63837c09e81700c1100401" + 
+			"8d9a9aeac0f6596f559c6d4daf59a5f2" + 
+			"6d9f200857ca6c3e9cac524bd9acc92a");
+
+    // Encrypt
+    var iv = util.hexStringToBinaryArray("000102030405060708090a0b0c0d0e0f");
+    var ofb = new OFBMode(new AESKey(key), new NullPad(), iv);
+    var src = ofb.encrypt(pt);
+    assert.deepEqual(ct, src);
+    
+    // Decrypt
+    var ofb = new OFBMode(new AESKey(key), new NullPad(), iv);
+    var decrypted = ofb.decrypt(src);
+    assert.deepEqual(pt, decrypted);
+    finished();
+  },  
+
+  "OFB AES-256 Test Vectors":function(assert, finished) {
+    var key = util.hexStringToBinaryArray("603deb1015ca71be2b73aef0857d7781" + 
+		  "1f352c073b6108d72d9810a30914dff4");		
+    var pt = util.hexStringToBinaryArray(
+			"6bc1bee22e409f96e93d7e117393172a" +
+			"ae2d8a571e03ac9c9eb76fac45af8e51" + 
+			"30c81c46a35ce411e5fbc1191a0a52ef" + 
+			"f69f2445df4f9b17ad2b417be66c3710");
+    var ct = util.hexStringToBinaryArray(
+			"dc7e84bfda79164b7ecd8486985d3860" + 
+			"4febdc6740d20b3ac88f6ad82a4fb08d" + 
+			"71ab47a086e86eedf39d1c5bba97c408" + 
+			"0126141d67f37be8538f5a8be740e484");
+
+    // Encrypt
+    var iv = util.hexStringToBinaryArray("000102030405060708090a0b0c0d0e0f");
+    var ofb = new OFBMode(new AESKey(key), new NullPad(), iv);
+    var src = ofb.encrypt(pt);
+    assert.deepEqual(ct, src);
+    
+    // Decrypt
+    var ofb = new OFBMode(new AESKey(key), new NullPad(), iv);
+    var decrypted = ofb.decrypt(src);
+    assert.deepEqual(pt, decrypted);
     finished();
   },  
 });
