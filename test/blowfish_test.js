e910592a98d8926ca8f9020c098c2672c88912e2
Christian Amor Kvalheim <christkv@gmail.com>
2011-02-24 17:46:18 +0100

    Finished up tests for streaming crypto

--- test/blowfish_test.js
+++ test/blowfish_test.js
@@ -63,10 +63,6 @@ suite.addTests({
   "Node Compatibility Tests":function(assert, finished) {
     var key = "018310DC409B26D6018310DC409B26D6";
     var pt =  "6bc1bee22e409f96";
-       // var pt = "6bc1bee22e409f96e93d7e117393172a" + 
-       //    "ae2d8a571e03ac9c9eb76fac45af8e51" + 
-       //    "30c81c46a35ce411e5fbc1191a0a52ef" + 
-       //    "f69f2445df4f9b17ad2b417be66c3710";
     // Encrypt using the pure js library    
     var iv = "0001020304050607";
 
@@ -147,75 +143,75 @@ suite.addTests({
     finished();    
   },
   
-  // "Streaming api test":function(assert, finished) {
-  //   var key = "603deb1015ca71be2b73aef0857d7781" + 
-  //      "1f352c073b6108d72d9810a30914dff4";         
-  //   // 5K of random data
-  //   var data = randomdata(1025);
-  //   // Blocksize
-  //   var blockSize = 32;
-  //   // Encrypt using the purejs librarie's streaming api in 1024 blocks
-  //   var iv = "000102030405060708090a0b0c0d0e0f";
-  //   var ofb = new OFBMode(new AESKey(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  // 
-  //   // Split the data
-  //   var numberOfBlocks = Math.floor(data.length / blockSize);
-  //   var leftOverbytes = data.length % blockSize;
-  //   var encryptedData = "";
-  // 
-  //   for(var i = 0; i < numberOfBlocks; i++) {
-  //     encryptedData += ofb.updateEncrypt(data.substr(i * blockSize, blockSize));
-  //   }    
-  // 
-  //   // If we have leftover bytes
-  //   if(leftOverbytes > 0) {
-  //     encryptedData += ofb.updateEncrypt(data.substr(data.length - leftOverbytes));      
-  //   }
-  //   // ok dokey let's finialize (ensuring we have the last padded block added)    
-  //   encryptedData += ofb.finalEncrypt();
-  // 
-  //   // Encrypt using node.js to ensure have have the same
-  //   var cipher = crypto.createCipheriv("aes-256-ofb", util.hexStringToBinary(key), util.hexStringToBinary(iv));    
-  //   var nodejsEncrypted = cipher.update(data, 'binary');
-  //   nodejsEncrypted += cipher.final('binary');
-  //   
-  //   // Verify encrypted streaming data
-  //   var a = util.binaryStringToArray(nodejsEncrypted);    
-  //   var b = util.binaryStringToArray(encryptedData);    
-  //   assert.deepEqual(b, a);
-  // 
-  //   // Decrypt the streaming data
-  //   var decipher = crypto.createDecipheriv("aes-256-ofb", util.hexStringToBinary(key), util.hexStringToBinary(iv));
-  //   var decryptedNode = decipher.update(encryptedData, 'binary');
-  //   decryptedNode += decipher.final('binary');    
-  //   // Decrypted content check for node.js
-  //   assert.deepEqual(util.binaryStringToArray(data), util.binaryStringToArray(decryptedNode));    
-  //     
-  //   // Clean cbc instance
-  //   ofb = new OFBMode(new AESKey(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));    
-  //   // Split the data
-  //   var numberOfBlocks = Math.floor(nodejsEncrypted.length / blockSize);
-  //   var leftOverbytes = nodejsEncrypted.length % blockSize;
-  //   var decryptedData = "";
-  //     
-  //   for(var i = 0; i < numberOfBlocks; i++) {
-  //     decryptedData += ofb.updateDecrypt(nodejsEncrypted.substr(i * blockSize, blockSize));
-  //   }    
-  //   
-  //   // Update with leftover bytes
-  //   if(leftOverbytes > 0) 
-  //     decryptedData += ofb.updateDecrypt(nodejsEncrypted.substr(numberOfBlocks*blockSize));          
-  // 
-  //   // ok dokey let's finialize (ensuring we have the last padded block added)    
-  //   decryptedData += ofb.finalDecrypt();
-  //     
-  //   // Verify encryption
-  //   var a = util.binaryStringToArray(decryptedNode);    
-  //   var b = util.binaryStringToArray(decryptedData);    
-  //   // Verify the decryption against node.js
-  //   assert.deepEqual(b, a);    
-  //   finished();
-  // },  
+  "Streaming api test":function(assert, finished) {
+    var key = "018310DC409B26D6018310DC409B26D6";
+    // Encrypt using the pure js library    
+    var iv = "0001020304050607";
+    // 5K of random data
+    var data = randomdata(1025);
+    // Blocksize
+    var blockSize = 32;
+    // Encrypt using the purejs librarie's streaming api in 1024 blocks
+    var ofb = new OFBMode(new BlowFish(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+  
+    // Split the data
+    var numberOfBlocks = Math.floor(data.length / blockSize);
+    var leftOverbytes = data.length % blockSize;
+    var encryptedData = "";
+  
+    for(var i = 0; i < numberOfBlocks; i++) {
+      encryptedData += ofb.updateEncrypt(data.substr(i * blockSize, blockSize));
+    }    
+  
+    // If we have leftover bytes
+    if(leftOverbytes > 0) {
+      encryptedData += ofb.updateEncrypt(data.substr(data.length - leftOverbytes));      
+    }
+    // ok dokey let's finialize (ensuring we have the last padded block added)    
+    encryptedData += ofb.finalEncrypt();
+  
+    // Encrypt using node.js to ensure have have the same
+    var cipher = crypto.createCipheriv("bf-ofb", util.hexStringToBinary(key), util.hexStringToBinary(iv));    
+    var nodejsEncrypted = cipher.update(data, 'binary');
+    nodejsEncrypted += cipher.final('binary');
+    
+    // Verify encrypted streaming data
+    var a = util.binaryStringToArray(nodejsEncrypted);    
+    var b = util.binaryStringToArray(encryptedData);    
+    assert.deepEqual(b, a);
+  
+    // Decrypt the streaming data
+    var decipher = crypto.createDecipheriv("bf-ofb", util.hexStringToBinary(key), util.hexStringToBinary(iv));
+    var decryptedNode = decipher.update(encryptedData, 'binary');
+    decryptedNode += decipher.final('binary');    
+    // Decrypted content check for node.js
+    assert.deepEqual(util.binaryStringToArray(data), util.binaryStringToArray(decryptedNode));    
+      
+    // Clean cbc instance
+    ofb = new OFBMode(new BlowFish(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));    
+    // Split the data
+    var numberOfBlocks = Math.floor(nodejsEncrypted.length / blockSize);
+    var leftOverbytes = nodejsEncrypted.length % blockSize;
+    var decryptedData = "";
+      
+    for(var i = 0; i < numberOfBlocks; i++) {
+      decryptedData += ofb.updateDecrypt(nodejsEncrypted.substr(i * blockSize, blockSize));
+    }    
+    
+    // Update with leftover bytes
+    if(leftOverbytes > 0) 
+      decryptedData += ofb.updateDecrypt(nodejsEncrypted.substr(numberOfBlocks*blockSize));          
+      
+    // ok dokey let's finialize (ensuring we have the last padded block added)    
+    decryptedData += ofb.finalDecrypt();
+      
+    // Verify encryption
+    var a = util.binaryStringToArray(decryptedNode);    
+    var b = util.binaryStringToArray(decryptedData);    
+    // Verify the decryption against node.js
+    assert.deepEqual(b, a);    
+    finished();
+  },  
 });
 
 
