80c1be29a5a171641fe49dea9d4c0250c08dab50
Christian Amor Kvalheim <christkv@gmail.com>
2011-03-20 18:19:07 +0100

    Finished implementing Serpent Cipher

--- test/serpent_test.js
+++ test/serpent_test.js
@@ -39,14 +39,6 @@ suite.addTests({
       "1032547698BADCFEEFCDAB8967452301", "81C4EB7B8AD9A8D0F2AA5D7BD626B560",
       "93DF9A3CAFE387BD999EEBE393A17FCA"];
     
-    // var keys = ["00112233445566778899AABBCCDDEEFFFFEEDDCCBBAA99887766554433221100"];
-    // var pts = ["1032547698BADCFEEFCDAB8967452301"];
-    // var cts = ["93DF9A3CAFE387BD999EEBE393A17FCA"];
-    // 
-    // var keys = ["00000000000000000000000000000000"];
-    // var pts = ["D29D576FCEA3A3A7ED9099F29273D78E"];
-    // var cts = ["B2288B968AE8B08648D1CE9606FD992D"];
-    
     // Test vectors
     for(var i = 0; i < keys.length; i++) {
       var key = util.hexStringToBinaryArray(keys[i]);
@@ -56,109 +48,109 @@ suite.addTests({
       // Encrypt the data and verify
       var serpent = new Serpent(key);
       var encrypted = serpent.encrypt(pt);
-      // assert.deepEqual(ct, encrypted);
-      // 
-      // // Decrypt data and verify
-      // serpent = new Serpent(key);
-      // var decrypted = serpent.decrypt(encrypted);
-      // assert.deepEqual(pt, decrypted);
+      assert.deepEqual(ct, encrypted);
+      
+      // Decrypt data and verify
+      serpent = new Serpent(key);
+      var decrypted = serpent.decrypt(encrypted);
+      assert.deepEqual(pt, decrypted);
     }
       
     finished();
   },  
 
-  // "Node Compatibility Tests":function(assert, finished) {
-  //   var key = "FBA167983E7AEF22317CE28C02AAE1A3E8E5CC3CEDBEA82A99DBC39AD65E7227";
-  //   var pt =  "02132435465768798a9bacbdcedfe0f1";
-  //   // Encrypt using the pure js library    
-  //   var iv = "0001020304050607";
-  //     
-  //   // OFB Mode
-  //   var ofb = new OFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  //   var src = ofb.encrypt(util.hexStringToBinaryArray(pt));
-  //     
-  //   var ofb = new OFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  //   var decryptedPureJs = ofb.decrypt(src);
-  //   assert.deepEqual(util.hexStringToBinaryArray(pt), decryptedPureJs);
-  //   
-  //   // CBC Mode
-  //   var cbc = new CBCMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  //   var src = cbc.encrypt(util.hexStringToBinaryArray(pt));
-  //   
-  //   var cbc = new CBCMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  //   var decryptedPureJs = cbc.decrypt(src);    
-  //   assert.deepEqual(util.hexStringToBinaryArray(pt), decryptedPureJs);
-  //   
-  //   // ECB Mode
-  //   var ecb = new ECBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  //   var src = ecb.encrypt(util.hexStringToBinaryArray(pt));
-  //   
-  //   var ecb = new ECBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  //   var decryptedPureJs = ecb.decrypt(src);    
-  //   assert.deepEqual(util.hexStringToBinaryArray(pt), decryptedPureJs);
-  //   
-  //   // CFB Mode
-  //   var ofb = new CFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  //   var src = ofb.encrypt(util.hexStringToBinaryArray(pt));
-  //   
-  //   var ofb = new CFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  //   var decryptedPureJs = ofb.decrypt(src);
-  //   assert.deepEqual(util.hexStringToBinaryArray(pt), decryptedPureJs);
-  //   finished();    
-  // },
-  // 
-  // "Streaming api test":function(assert, finished) {
-  //   var key = "FBA167983E7AEF22317CE28C02AAE1A3E8E5CC3CEDBEA82A99DBC39AD65E7227";
-  //   // Encrypt using the pure js library    
-  //   var iv = "0001020304050607";
-  //   // 5K of random data
-  //   var data = randomdata(1025);
-  //   // Blocksize
-  //   var blockSize = 32;
-  //   // Encrypt using the purejs librarie's streaming api in 1024 blocks
-  //   var ofb = new OFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  // 
-  //   // Split the data
-  //   var numberOfBlocks = Math.floor(data.length / blockSize);
-  //   var leftOverbytes = data.length % blockSize;
-  //   var encryptedData = "";
-  // 
-  //   for(var i = 0; i < numberOfBlocks; i++) {
-  //     encryptedData += ofb.updateEncrypt(data.substr(i * blockSize, blockSize));
-  //   }    
-  // 
-  //   // If we have leftover bytes
-  //   if(leftOverbytes > 0) {
-  //     encryptedData += ofb.updateEncrypt(data.substr(data.length - leftOverbytes));      
-  //   }
-  //   // ok dokey let's finialize (ensuring we have the last padded block added)    
-  //   encryptedData += ofb.finalEncrypt();
-  //   
-  //   // Single pass encryption
-  //   ofb = new OFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
-  //   src = ofb.encrypt(util.binaryStringToArray(data));
-  //   assert.deepEqual(src, util.binaryStringToArray(encryptedData));
-  //       
-  //   // Clean cbc instance
-  //   ofb = new OFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));    
-  //   // Split the data
-  //   var numberOfBlocks = Math.floor(src.length / blockSize);
-  //   var leftOverbytes = src.length % blockSize;
-  //   var decryptedData = "";
-  //     
-  //   for(var i = 0; i < numberOfBlocks; i++) {
-  //     decryptedData += ofb.updateDecrypt(util.arrayToBinaryString(src).substr(i * blockSize, blockSize));
-  //   }    
-  //   
-  //   // Update with leftover bytes
-  //   if(leftOverbytes > 0) 
-  //     decryptedData += ofb.updateDecrypt(util.arrayToBinaryString(src).substr(numberOfBlocks*blockSize));          
-  //     
-  //   // ok dokey let's finialize (ensuring we have the last padded block added)    
-  //   decryptedData += ofb.finalDecrypt();
-  // 
-  //   // Compare
-  //   assert.deepEqual(util.binaryStringToArray(data), util.binaryStringToArray(decryptedData))    
-  //   finished();
-  // },
+  "Node Compatibility Tests":function(assert, finished) {
+    var key = "00112233445566778899AABBCCDDEEFFFFEEDDCCBBAA99887766554433221100";
+    var pt =  "02132435465768798a9bacbdcedfe0f1";
+    // Encrypt using the pure js library    
+    var iv = "0001020304050607";
+      
+    // OFB Mode
+    var ofb = new OFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+    var src = ofb.encrypt(util.hexStringToBinaryArray(pt));
+      
+    var ofb = new OFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+    var decryptedPureJs = ofb.decrypt(src);
+    assert.deepEqual(util.hexStringToBinaryArray(pt), decryptedPureJs);
+    
+    // CBC Mode
+    var cbc = new CBCMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+    var src = cbc.encrypt(util.hexStringToBinaryArray(pt));
+    
+    var cbc = new CBCMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+    var decryptedPureJs = cbc.decrypt(src);    
+    assert.deepEqual(util.hexStringToBinaryArray(pt), decryptedPureJs);
+    
+    // ECB Mode
+    var ecb = new ECBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+    var src = ecb.encrypt(util.hexStringToBinaryArray(pt));
+    
+    var ecb = new ECBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+    var decryptedPureJs = ecb.decrypt(src);    
+    assert.deepEqual(util.hexStringToBinaryArray(pt), decryptedPureJs);
+    
+    // CFB Mode
+    var ofb = new CFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+    var src = ofb.encrypt(util.hexStringToBinaryArray(pt));
+    
+    var ofb = new CFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+    var decryptedPureJs = ofb.decrypt(src);
+    assert.deepEqual(util.hexStringToBinaryArray(pt), decryptedPureJs);
+    finished();    
+  },
+  
+  "Streaming api test":function(assert, finished) {
+    var key = "FBA167983E7AEF22317CE28C02AAE1A3E8E5CC3CEDBEA82A99DBC39AD65E7227";
+    // Encrypt using the pure js library    
+    var iv = "0001020304050607";
+    // 5K of random data
+    var data = randomdata(1025);
+    // Blocksize
+    var blockSize = 32;
+    // Encrypt using the purejs librarie's streaming api in 1024 blocks
+    var ofb = new OFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+  
+    // Split the data
+    var numberOfBlocks = Math.floor(data.length / blockSize);
+    var leftOverbytes = data.length % blockSize;
+    var encryptedData = "";
+  
+    for(var i = 0; i < numberOfBlocks; i++) {
+      encryptedData += ofb.updateEncrypt(data.substr(i * blockSize, blockSize));
+    }    
+  
+    // If we have leftover bytes
+    if(leftOverbytes > 0) {
+      encryptedData += ofb.updateEncrypt(data.substr(data.length - leftOverbytes));      
+    }
+    // ok dokey let's finialize (ensuring we have the last padded block added)    
+    encryptedData += ofb.finalEncrypt();
+    
+    // Single pass encryption
+    ofb = new OFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));
+    src = ofb.encrypt(util.binaryStringToArray(data));
+    assert.deepEqual(src, util.binaryStringToArray(encryptedData));
+        
+    // Clean cbc instance
+    ofb = new OFBMode(new Serpent(util.hexStringToBinaryArray(key)), null, util.hexStringToBinaryArray(iv));    
+    // Split the data
+    var numberOfBlocks = Math.floor(src.length / blockSize);
+    var leftOverbytes = src.length % blockSize;
+    var decryptedData = "";
+      
+    for(var i = 0; i < numberOfBlocks; i++) {
+      decryptedData += ofb.updateDecrypt(util.arrayToBinaryString(src).substr(i * blockSize, blockSize));
+    }    
+    
+    // Update with leftover bytes
+    if(leftOverbytes > 0) 
+      decryptedData += ofb.updateDecrypt(util.arrayToBinaryString(src).substr(numberOfBlocks*blockSize));          
+      
+    // ok dokey let's finialize (ensuring we have the last padded block added)    
+    decryptedData += ofb.finalDecrypt();
+  
+    // Compare
+    assert.deepEqual(util.binaryStringToArray(data), util.binaryStringToArray(decryptedData))    
+    finished();
+  },
 });
